69a984123576cc4129f7f14e4b05beae62e3afd0
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-04-06 14:25:04 +0100

    added the payment page

--- src/pages/payment-page/payment-page.jsx
+++ src/pages/payment-page/payment-page.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Text, Button } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+
 import Layout from "../../components/layout/layout-component";
+import { PaymentContainer } from "./payment-page.styles";
 
 const PaymentPage = () => {
-  return <Layout>
-    
-  </Layout>;
+  return (
+    <Layout>
+      <PaymentContainer>
+        <div className="payment-row">
+          <Text>Departmental Dues</Text>
+          <span>$50</span>
+        </div>
+
+        <Button rightIcon={<ExternalLinkIcon />} colorScheme="teal" className="" mb={10} size="lg">
+          PAY NOW
+        </Button>
+
+        <div className="payment-row">
+          <Text>Faculty Dues</Text>
+          <span>$100</span>
+        </div>
+        <Button rightIcon={<ExternalLinkIcon />} colorScheme="teal" className="" mb={10} size="lg">
+          PAY NOW
+        </Button>
+
+
+        <div className="payment-row">
+          <Text>Tuition Fees</Text>
+          <span>$1000</span>
+        </div>
+        <Button rightIcon={<ExternalLinkIcon />} colorScheme="teal" className="" size="lg">
+          PAY NOW
+        </Button>
+      </PaymentContainer>
+    </Layout>
+  );
 };
 
 export default PaymentPage;
---
+++ src/pages/payment-page/payment-page.styles.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const PaymentContainer = styled.div`
+  background-color: #f1f1f2;
+  width: 90vw;
+  height: 92vh;
+  font-size: 2rem;
+  color: teal;
+  padding: 10rem;
+
+  .payment-row {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+  }
+
+  @media only screen and (max-width: 800px) {
+    width: 82vw;
+    padding: 5rem;
+  }
+  @media only screen and (max-width: 600px) {
+    width: 100vw;
+  }
+`;
